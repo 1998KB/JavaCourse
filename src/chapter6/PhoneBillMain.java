1cfcb3bd6fa7fdf026c85fa07a587e79f0591edd
1998KB <klajdibeqiraj88@gmail.com>
2022-12-15 13:13:40 +0100

    second commit chapter 6

--- src/chapter6/PhoneBillMain.java
+++ src/chapter6/PhoneBillMain.java
@@ -1,2 +1,13 @@
-package chapter6;public class PhoneBillMain {
-}
+package chapter6;
+
+public class PhoneBillMain {
+        public static void main(String args[]){
+            PhoneBill bill = new PhoneBill();
+            bill.setId(23456);
+            bill.setBaseCost(40);
+            bill.setAllottedMinutes(85);
+            bill.setMinutesUsed(90);
+            bill.printItemizedBill();
+        }
+    }
+
