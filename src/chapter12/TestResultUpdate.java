52a9927d3b9c9684ce60709b30baec60a4b5f6a7
1998KB <klajdibeqiraj88@gmail.com>
2022-12-16 18:40:45 +0100

    cp 12

---
+++ src/chapter12/TestResultUpdate.java
@@ -0,0 +1,27 @@
+package chapter12;
+
+import java.util.Map;
+
+public class TestResultUpdate extends TestResult{
+
+    public static void main(String[] arg){
+
+        Map <String, Integer> original = TestResult.getOriginalGrades();
+        Map <String, Integer> update = TestResult.getMakeUpGrades();
+
+        for (var student : update.entrySet()){
+            Integer firstGrade = original.get(student.getKey());
+            Integer secondGrade = update.get(student.getKey());
+
+            if (secondGrade < firstGrade){
+                update.put(student.getKey(), firstGrade);
+                System.out.println("Student: " + student.getKey() + ", Grade:" + update.get(student.getKey()));
+            }
+        }
+
+
+
+
+    }
+
+}
