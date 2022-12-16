92d0fe18a4b6b5305d024839d464f9091f01ad55
1998KB <klajdibeqiraj88@gmail.com>
2022-12-16 21:16:10 +0100

    cp 13,14

---
+++ src/chapter14/CoinTossGame.java
@@ -0,0 +1,53 @@
+package chapter14;
+
+import java.util.Scanner;
+
+public class CoinTossGame {
+    private Scanner scanner;
+    public static void main(String[] arg) {
+        CoinTossGame game = new CoinTossGame();
+        game.scanner = new Scanner(System.in);
+
+        Player player1 = new Player(game.askPlayerName());
+        player1.setGuess((game.askGuess()));
+
+        Player player2 = new Player(game.askPlayerName());
+
+        if(player1.getGuess().equalsIgnoreCase(Coin.HEADS)){
+            player2.setGuess(Coin.TAILS);
+        }else{
+            player2.setGuess(Coin.HEADS);
+        }
+
+        System.out.println("Flipping the coin...");
+        Coin coin = new Coin();
+        coin.flip();
+        System.out.println("The coin landed on " + coin.getSide());
+
+        game.determineWinner(player1, player2, coin);
+        game.scanner.close();
+    }
+
+    public  String askPlayerName(){
+        System.out.println("What is your name? ");
+        return scanner.next();
+    }
+
+    public String askGuess(){
+        String prompt = String.format("%s or %s?", Coin.HEADS, Coin.TAILS);
+        System.out.println(prompt);
+        return scanner.next();
+    }
+
+    public void determineWinner(Player player1, Player player2, Coin coin){
+        String winner;
+        if(coin.getSide().equalsIgnoreCase(player1.getGuess())){
+            winner = player1.getName();
+        }
+        else{
+            winner = player2.getName();
+        }
+
+        System.out.println("The winner is " + winner);
+    }
+}
